844d0cb04fd4e37944ebd255d94cfe18c071d135
Erik C. Olson <erikcaineolson@gmail.com>
2016-08-08 19:10:58 -0700

    Initial "requests" file.
    TODO: ...build this...

---
+++ middleware/requests.js
@@ -0,0 +1,10 @@
+const https = require('https');
+const config = require('../config/config');
+const maps = require('maps');
+var Slack = require('slack-node');
+
+function askTravelBot() {
+    var slack = new Slack();
+
+
+}
